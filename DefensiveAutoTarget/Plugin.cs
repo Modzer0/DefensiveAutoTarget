b6e2b9728c99a2e28ecd699ccea7ad7b41c91997
Marshall Of Storms <brandon.r.jasper@gmail.com>
2026-04-25 09:41:21 -0400

    Require host to have mod installed for feature to function

--- DefensiveAutoTarget/Plugin.cs
+++ DefensiveAutoTarget/Plugin.cs
@@ -1,18 +1,31 @@
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using NuclearOption.Networking;
+using NuclearOption.Networking.Lobbies;
+using Steamworks;
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace DefensiveAutoTarget
 {
-    [BepInPlugin("com.defensiveautotarget", "Defensive Auto Target", "0.5.0")]
+    [BepInPlugin(PluginGUID, "Defensive Auto Target", "0.5.0")]
     public class Plugin : BaseUnityPlugin
     {
+        public const string PluginGUID = "com.defensiveautotarget";
+        private const string LobbyDataKey = "dat_mod";
+        private const string LobbyDataValue = "0.5.0";
+
         public static ConfigEntry<KeyCode> AutoTargetKey;
+        public static bool HostHasMod;
+        public static Plugin Instance;
         private Harmony _harmony;
 
         private void Awake()
         {
+            Instance = this;
+
             AutoTargetKey = Config.Bind(
                 "Controls",
                 "autoTargetMissileKey",
@@ -20,10 +33,44 @@ namespace DefensiveAutoTarget
                 "Key to automatically target the nearest incoming missile. Set to None to disable."
             );
 
-            _harmony = new Harmony("com.defensiveautotarget");
+            _harmony = new Harmony(PluginGUID);
             _harmony.PatchAll();
 
             Logger.LogInfo("Defensive Auto Target loaded.");
+        }
+
+        private static CSteamID GetCurrentLobby()
+        {
+            try
+            {
+                if (SteamLobby.instance == null)
+                    return new CSteamID();
+
+                var hostedLobby = (HostedLobbyInstance)AccessTools
+                    .Field(typeof(SteamLobby), "_hostedLobby")
+                    .GetValue(SteamLobby.instance);
+
+                if (hostedLobby.IsValid)
+                    return hostedLobby.Id;
+
+                var joinedLobby = (LobbyInstance)AccessTools
+                    .Field(typeof(SteamLobby), "_joinedLobby")
+                    .GetValue(SteamLobby.instance);
+
+                if (joinedLobby is PlayerLobbyInstance playerLobby)
+                {
+                    var prop = AccessTools.Property(typeof(PlayerLobbyInstance), "LobbyId");
+                    if (prop != null)
+                    {
+                        var id = (CSteamID)prop.GetValue(playerLobby);
+                        if (id.m_SteamID != 0UL)
+                            return id;
+                    }
+                }
+            }
+            catch (Exception) { }
+
+            return new CSteamID();
         }
 
         public static void AutoTargetNearestMissile(Aircraft aircraft)
@@ -48,12 +95,70 @@ namespace DefensiveAutoTarget
             combatHud.SelectUnit(missile);
         }
 
+        // Host: advertise mod presence in lobby data when hosting
+        [HarmonyPatch(typeof(NetworkManagerNuclearOption), "StartHostAsync")]
+        private class HostAdvertisePatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix(NetworkManagerNuclearOption __instance)
+            {
+                __instance.StartCoroutine(SetLobbyData());
+            }
+
+            private static IEnumerator SetLobbyData()
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    yield return new WaitForSeconds(1f);
+                    var lobbyId = GetCurrentLobby();
+                    if (lobbyId.m_SteamID != 0UL)
+                    {
+                        SteamMatchmaking.SetLobbyData(lobbyId, LobbyDataKey, LobbyDataValue);
+                        HostHasMod = true;
+                        yield break;
+                    }
+                }
+            }
+        }
+
+        // Client: check lobby data after joining to see if host has the mod
+        [HarmonyPatch(typeof(SteamLobby), "TryJoinLobby")]
+        private class ClientCheckPatch
+        {
+            [HarmonyPostfix]
+            private static void Postfix()
+            {
+                if (Instance != null)
+                    Instance.StartCoroutine(CheckHostMod());
+            }
+
+            private static IEnumerator CheckHostMod()
+            {
+                HostHasMod = false;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    yield return new WaitForSeconds(1f);
+                    var lobbyId = GetCurrentLobby();
+                    if (lobbyId.m_SteamID != 0UL)
+                    {
+                        string val = SteamMatchmaking.GetLobbyData(lobbyId, LobbyDataKey);
+                        HostHasMod = !string.IsNullOrEmpty(val);
+                        yield break;
+                    }
+                }
+            }
+        }
+
+        // Input patch: only fire if host has the mod
         [HarmonyPatch(typeof(PilotPlayerState), "PlayerControls")]
         private class AutoTargetMissilePatch
         {
             [HarmonyPostfix]
             private static void Postfix()
             {
+                if (!HostHasMod)
+                    return;
                 if (AutoTargetKey.Value == KeyCode.None)
                     return;
                 if (!Input.GetKeyDown(AutoTargetKey.Value))
